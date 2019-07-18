ede9193e27b9b4ec8bf40d23a95198c0b458fce8
Amarit Jarasjindarat <amarit.jarasjindarat@gmail.com>
2019-07-18 16:31:58 +0700

    change service id

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '29886d7a-9b47-4004-9c00-645847f5e704'; // LED, Button
+const USER_SERVICE_UUID         = '3abd51af-83ea-4520-a175-e933319cb517'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
